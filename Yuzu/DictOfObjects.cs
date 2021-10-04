3f2aa368126d8410fb3403ed8f03cff4f47e9103
Igor Fomin <ifomin@game-forest.com>
2021-10-04 11:28:22 +1000

    DictOfObjects: Use property alias instead of names. Use object type instead of typeof(T)

--- Yuzu/DictOfObjects.cs
+++ Yuzu/DictOfObjects.cs
@@ -10,14 +10,14 @@ namespace Yuzu.DictOfObjects
 		public static CommonOptions Options = new CommonOptions();
 
 		public static Dictionary<string, object> Pack<T>(T obj) =>
-			Meta.Get(typeof(T), Options).Items.ToDictionary(yi => yi.Name, yi => yi.GetValue(obj));
+			Meta.Get(obj.GetType(), Options).Items.ToDictionary(yi => yi.Tag(Options), yi => yi.GetValue(obj));
 
 		public static T Unpack<T>(Dictionary<string, object> d)
 		{
 			var meta = Meta.Get(typeof(T), Options);
 			var result = meta.Factory();
 			foreach (var yi in meta.Items) {
-				if (d.TryGetValue(yi.Name, out object itemValue))
+				if (d.TryGetValue(yi.Tag(Options), out object itemValue))
 					yi.SetValue(result, itemValue);
 			}
 			return (T)result;
