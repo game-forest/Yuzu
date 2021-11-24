db1734c05db1ebc221fad7b4293726df28a9af96
Mike Rojkov <mikhail.rozhkov@gmail.com>
2021-11-24 18:31:18 +1000

    [CIT-2000] Move ReferenceResolver class into TestReferences.cs

--- Yuzu/ReferenceResolver.cs
+++ Yuzu/ReferenceResolver.cs
@@ -16,41 +16,4 @@ namespace Yuzu
 		Type ReferenceType();
 		bool TryGetReference(object obj, out object reference, out bool referenceGenerated);
 	}
-
-	public class ReferenceResolver : IDeserializerReferenceResolver, ISerializerReferenceResolver
-	{
-		private readonly Dictionary<int, object> referenceToObjects = new Dictionary<int, object>();
-		private readonly Dictionary<object, int> objectsToReferences = new Dictionary<object, int>();
-		private int currentId = 1;
-
-		public Type ReferenceType() => typeof(int);
-
-		public object GetObject(object reference)
-		{
-			if (!referenceToObjects.TryGetValue((int)reference, out var obj)) {
-				throw new YuzuException($"Unresolved reference: {reference}");
-			}
-			return obj;
-		}
-
-		public bool TryGetObject(object reference, out object obj)
-		{
-			return referenceToObjects.TryGetValue((int)reference, out obj);
-		}
-
-		public void AddObject(object reference, object obj) => referenceToObjects.Add((int)reference, obj);
-
-		public bool TryGetReference(object obj, out object reference, out bool referenceGenerated)
-		{
-			if (objectsToReferences.TryGetValue(obj, out var key)) {
-				reference = key;
-				referenceGenerated = false;
-			} else {
-				reference = currentId++;
-				objectsToReferences.Add(obj, (int)reference);
-				referenceGenerated = true;
-			}
-			return true;
-		}
-	}
 }
