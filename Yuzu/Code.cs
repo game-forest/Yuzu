9e4d81e0662f29233cc578233dd8bff88a311c3b
Denis Rotanov <mapholameth@gmail.com>
2023-03-20 14:54:23 +0400

    implement code serializer

--- Yuzu/Code.cs
+++ Yuzu/Code.cs
@@ -1,5 +1,6 @@
-﻿using System;
-
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Yuzu.Metadata;
 using Yuzu.Util;
 
@@ -9,32 +10,112 @@ namespace Yuzu.Code
 	{
 		public string VarName = "x";
 		public string Indent = "\t";
+		public string NewLine = "\n";
 	}
 
 	public class CodeConstructSerializer : AbstractStringSerializer
 	{
 		public CodeConstructSerializeOptions CodeConstructOptions = new CodeConstructSerializeOptions();
 
-		protected override void ToBuilder(object obj)
+		private bool IsPrimitive(object value)
 		{
-			builder.AppendFormat("var {0} = new {1} {{\n", CodeConstructOptions.VarName, obj.GetType().Name);
+			if (value == null) {
+				return true;
+			}
+			var t = value.GetType();
+			return t.IsPrimitive
+				|| t.IsEnum
+				|| t == typeof(string)
+				|| t == typeof(decimal)
+				|| t == typeof(DateTime)
+				|| t == typeof(DateTimeOffset)
+				|| t == typeof(TimeSpan)
+				|| t == typeof(Guid);
+		}
+
+		protected override void ToBuilder(object value)
+		{
+			var o = CodeConstructOptions;
+			builder.Append($"var {o.VarName} = ");
+			AppendValue(value);
+			builder.Append($";{o.NewLine}");
+		}
+
+		private void AppendValue(object value)
+		{
+			var t = value?.GetType();
+			if (IsPrimitive(value)) {
+				AppendPrimitiveValue(value);
+			} else if (Utils.GetICollection(t) != null) {
+				AppendCollection(value);
+			} else if (Utils.GetIDictionary(t) != null) {
+				AppendDictionary(value);
+			} else {
+				AppendCompositeValue(value);
+			}
+		}
+
+		private void AppendDictionary(object value)
+		{
+			var o = CodeConstructOptions;
+			builder.Append($"new {Utils.GetTypeSpec(value.GetType())} {{{o.NewLine}");
 			var first = true;
-			foreach (var yi in Meta.Get(obj.GetType(), Options).Items) {
+			foreach (DictionaryEntry e in (IDictionary)value) {
 				if (!first) {
-					builder.Append(",\n");
+					builder.Append($",{o.NewLine}");
+				}
+				first = false;
+				builder.Append(CodeConstructOptions.Indent);
+				builder.Append("{{");
+				AppendValue(e.Key);
+				builder.Append(", ");
+				AppendValue(e.Value);
+				builder.Append("}}");
+			}
+			builder.Append($"{o.NewLine}}}");
+		}
+
+		private void AppendCollection(object value)
+		{
+			var o = CodeConstructOptions;
+			builder.Append($"new {Utils.GetTypeSpec(value.GetType())} {{{o.NewLine}");
+			var first = true;
+			foreach (var e in (ICollection)value) {
+				if (!first) {
+					builder.Append($",{o.NewLine}");
+				}
+				first = false;
+				builder.Append(CodeConstructOptions.Indent);
+				AppendValue(e);
+			}
+			builder.Append($"{o.NewLine}}}");
+		}
+
+		private void AppendPrimitiveValue(object value)
+		{
+			var v = Utils.CodeValueFormat(value);
+			builder.Append(v);
+		}
+
+		private void AppendCompositeValue(object value)
+		{
+			var o = CodeConstructOptions;
+			builder.Append($"new {Utils.GetTypeSpec(value.GetType())} {{{o.NewLine}");
+			var first = true;
+			foreach (var yi in Meta.Get(value.GetType(), Options).Items) {
+				if (!first) {
+					builder.Append($",{o.NewLine}");
+				}
+				if (yi.SetValue == null) {
+					continue;
 				}
 				first = false;
 				builder.Append(CodeConstructOptions.Indent);
 				builder.Append(yi.Name);
 				builder.Append(" = ");
-				var v = Utils.CodeValueFormat(yi.GetValue(obj));
-				if (v == string.Empty) {
-					throw new NotImplementedException(yi.Type.Name);
-				}
-
-				builder.Append(v);
+				AppendValue(yi.GetValue(value));
 			}
-			builder.Append("\n};\n");
+			builder.Append($"{o.NewLine}}}");
 		}
 	}
 
