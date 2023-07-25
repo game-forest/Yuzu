3e9e37fa170fee17e5d8bceba4315db5d9d35067
Konstantin Egorov <kegorov@khagheriantar.am>
2023-07-25 12:12:24 +0400

    [Yuzu] Fix tests

--- Yuzu/YuzuTest/TestReferences.cs
+++ Yuzu/YuzuTest/TestReferences.cs
@@ -29,9 +29,9 @@ namespace YuzuTest
 			tyler.DirectReports = new List<IEmployee> { adrian };
 			adrian.Manager = tyler;
 			var s =
-				"{\n\"id\":1,\n\"class\":\"YuzuTest.TestReferences+Employee, YuzuTest\",\n" +
-				"\"DirectReports\":[\n{\n\"id\":2,\n\"class\":\"YuzuTest.TestReferences+Employee, YuzuTest\",\n" +
-				"\"Manager\":{\n\"ref\":1\n},\n\"Name\":\"Adrian King\"\n}\n],\n\"Name\":\"Tyler Stein\"\n}";
+				"{\n\"id\":0,\n\"class\":\"YuzuTest.TestReferences+Employee, YuzuTest\",\n" +
+				"\"DirectReports\":[\n{\n\"id\":1,\n\"class\":\"YuzuTest.TestReferences+Employee, YuzuTest\",\n" +
+				"\"Manager\":{\n\"ref\":0\n},\n\"Name\":\"Adrian King\"\n}\n],\n\"Name\":\"Tyler Stein\"\n}";
 			Assert.AreEqual(js.ToString(tyler), s);
 		}
 
@@ -46,9 +46,9 @@ namespace YuzuTest
 			tyler.DirectReports = new List<IEmployee> { adrian };
 			adrian.Manager = tyler;
 			var s = 
-				"20-FF-FF-01-00-00-00-01-00-2A-59-75-7A-75-54-65-73-74-2E-54-65-73-74-52-65-66-65-72-65-6E-63-65-73-2B-45-6D-70-" +
+				"20-FF-FF-00-00-00-00-01-00-2A-59-75-7A-75-54-65-73-74-2E-54-65-73-74-52-65-66-65-72-65-6E-63-65-73-2B-45-6D-70-" +
 				"6C-6F-79-65-65-2C-20-59-75-7A-75-54-65-73-74-00-03-00-0D-44-69-72-65-63-74-52-65-70-6F-72-74-73-21-20-07-4D-61-" +
-				"6E-61-67-65-72-20-04-4E-61-6D-65-10-01-00-01-00-00-00-FF-FF-02-00-00-00-01-00-02-00-FE-FF-01-00-00-00-03-00-0B-" +
+				"6E-61-67-65-72-20-04-4E-61-6D-65-10-01-00-01-00-00-00-FF-FF-01-00-00-00-01-00-02-00-FE-FF-00-00-00-00-03-00-0B-" +
 				"41-64-72-69-61-6E-20-4B-69-6E-67-00-00-03-00-0B-54-79-6C-65-72-20-53-74-65-69-6E-00-00";
 			Assert.AreEqual(BitConverter.ToString(bs.ToBytes(tyler)), s);
 		}
@@ -74,6 +74,7 @@ namespace YuzuTest
 			}
 		}
 
+		[YuzuSerializeByReference]
 		public class Node
 		{
 			[YuzuMember]
@@ -111,6 +112,7 @@ namespace YuzuTest
 			public List<IEmployee> DirectReports { get; set; }
 		}
 
+		[YuzuSerializeByReference]
 		public class Employee : IEmployee
 		{
 			public string Name { get; set; }
@@ -135,13 +137,8 @@ namespace YuzuTest
 					},
 					ReferenceResolver = new ReferenceResolver()
 				};
-				try {
-					var buf = s.ToString(i);
-					return d.FromString(buf);
-				} finally {
-					Assert.IsTrue(((ReferenceResolver)s.ReferenceResolver).IsEmpty());
-					Assert.IsTrue(((ReferenceResolver)d.ReferenceResolver).IsEmpty());
-				}
+				var buf = s.ToString(i);
+				return d.FromString(buf);
 			},
 			// Binary
 			i => {
@@ -151,13 +148,8 @@ namespace YuzuTest
 				var d = new BinaryDeserializer {
 					ReferenceResolver = new ReferenceResolver()
 				};
-				try {
-					var buf = s.ToBytes(i);
-					return d.FromBytes(buf);
-				} finally {
-					Assert.IsTrue(((ReferenceResolver)s.ReferenceResolver).IsEmpty());
-					Assert.IsTrue(((ReferenceResolver)d.ReferenceResolver).IsEmpty());
-				}
+				var buf = s.ToBytes(i);
+				return d.FromBytes(buf);
 			},
 			// Binary generated
 			i => {
@@ -167,118 +159,55 @@ namespace YuzuTest
 				var d = new YuzuGenBin.BinaryDeserializerGenDerived {
 					ReferenceResolver = new ReferenceResolver()
 				};
-				try {
-					var buf = s.ToBytes(i);
-					return d.FromBytes(buf);
-				} finally {
-					Assert.IsTrue(((ReferenceResolver)s.ReferenceResolver).IsEmpty());
-					Assert.IsTrue(((ReferenceResolver)d.ReferenceResolver).IsEmpty());
-				}
+				var buf = s.ToBytes(i);
+				return d.FromBytes(buf);
 			},
 			// Cloner
 			i => {
 				var c = new Cloner {
 					ReferenceResolver = new ReferenceResolver()
 				};
-				try {
-					return c.DeepObject(i);
-				} finally {
-					Assert.IsTrue(((ReferenceResolver)c.ReferenceResolver).IsEmpty());
-				}
+				return c.DeepObject(i);
 			},
 			// Cloner generated
 			i => {
 				var c = new YuzuGenClone.ClonerGenDerived {
 					ReferenceResolver = new ReferenceResolver()
 				};
-				try {
-					return c.DeepObject(i);
-				} finally {
-					Assert.IsTrue(((ReferenceResolver)c.ReferenceResolver).IsEmpty());
-				}
-			},
-			// Cloner with optimized reference resolver
-			i => {
-				var c = new Cloner {
-					ReferenceResolver = new ReferenceResolverOptimizedForCloner()
-				};
-				try {
-					return c.DeepObject(i);
-				} finally {
-					Assert.IsTrue(((ReferenceResolverOptimizedForCloner)c.ReferenceResolver).IsEmpty());
-				}
+				return c.DeepObject(i);
 			},
 		};
 
-		private class ReferenceResolver : IDeserializerReferenceResolver, ISerializerReferenceResolver, IClonerReferenceResolver
+		private class ReferenceResolver : ReferenceResolver<int>
 		{
-			private readonly Dictionary<int, object> referenceToObjects = new Dictionary<int, object>();
-			private readonly Dictionary<object, int> objectsToReferences = new Dictionary<object, int>();
-			private int currentId = 1;
+			private readonly List<object> referenceToObject = new List<object>();
+			private readonly Dictionary<object, int> objectToReference = new Dictionary<object, int>();
 
-			public Type ReferenceType() => typeof(int);
-
-			public object GetObject(object reference)
+			public override int GetReference(object obj, out bool alreadyExists)
 			{
-				if (!referenceToObjects.TryGetValue((int)reference, out var obj)) {
+				alreadyExists = objectToReference.TryGetValue(obj, out var reference);
+				if (!alreadyExists) {
+					reference = objectToReference.Count;
+					objectToReference.Add(obj, reference);
+				}
+				return reference;
+			}
+
+			public override void AddReference(int reference, object obj)
+			{
+				while (referenceToObject.Count <= reference) {
+					referenceToObject.Add(null);
+				}
+				referenceToObject[reference] = obj;
+			}
+
+			public override object ResolveReference(int reference)
+			{
+				if (reference < 0 || reference >= referenceToObject.Count || referenceToObject[reference] == null) {
 					throw new YuzuException($"Unresolved reference: {reference}");
 				}
-				return obj;
+				return referenceToObject[reference];
 			}
-
-			public bool TryGetObject(object reference, out object obj)
-			{
-				return referenceToObjects.TryGetValue((int)reference, out obj);
-			}
-
-			public void AddObject(object reference, object obj) => referenceToObjects.Add((int)reference, obj);
-
-			public bool TryGetReference(object obj, object owner, out object reference, out bool writeObject)
-			{
-				if (objectsToReferences.TryGetValue(obj, out var key)) {
-					reference = key;
-					writeObject = false;
-				} else {
-					reference = currentId++;
-					objectsToReferences.Add(obj, (int)reference);
-					writeObject = true;
-				}
-				return true;
-			}
-
-			public bool TryGetReference(object obj, out object reference, out bool newReference)
-			{
-				return TryGetReference(obj, null, out reference, out newReference);
-			}
-
-			public void Clear()
-			{
-				referenceToObjects.Clear();
-				objectsToReferences.Clear();
-				currentId = 1;
-			}
-
-			public bool IsEmpty() => referenceToObjects.Count == 0 && objectsToReferences.Count == 0;
-		}
-
-		private class ReferenceResolverOptimizedForCloner : IClonerReferenceResolver
-		{
-			private readonly Dictionary<object, object> sourceToClone = new Dictionary<object, object>();
-
-			public void AddObject(object reference, object clone) => sourceToClone.Add(reference, clone);
-
-			public void Clear() => sourceToClone.Clear();
-
-			public object GetObject(object reference) => sourceToClone[reference];
-
-			public bool TryGetReference(object source, out object reference, out bool newReference)
-			{
-				reference = source;
-				newReference = !sourceToClone.ContainsKey(source);
-				return true;
-			}
-
-			public bool IsEmpty() => sourceToClone.Count == 0;
 		}
 	}
 }
